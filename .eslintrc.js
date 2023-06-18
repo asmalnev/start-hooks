7c2b7e5ae44f8d2027dff9c7db5c51d3012cf883
Aleksandr Malnev <asmalnev@gmail.com>
2023-06-18 16:49:46 +0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ["plugin:react/recommended", "standard"],
+  overrides: [],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    indent: [0, 2],
+    semi: [2, "always"],
+    "space-before-function-paren": ["error", "never"],
+    quotes: ["error", "double", { allowTemplateLiterals: true }],
+    "multiline-ternary": "off"
+  }
+};
